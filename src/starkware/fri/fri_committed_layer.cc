d1d08e73bc4cec7cbde9ceaaf60168cdd8f9efb7
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-10-03 15:17:26 -0300

    Add more logs

--- src/starkware/fri/fri_committed_layer.cc
+++ src/starkware/fri/fri_committed_layer.cc
@@ -26,8 +26,10 @@ using starkware::fri::details::SecondLayerQeuriesToFirstLayerQueries;
 //  ---------------------------------------------------------
 //  Class FriCommittedLayerByCallback.
 
-void FriCommittedLayerByCallback::Decommit(const std::vector<uint64_t>& queries) {
-  (*layer_queries_callback_)(SecondLayerQeuriesToFirstLayerQueries(queries, fri_step_));
+void FriCommittedLayerByCallback::Decommit(
+    const std::vector<uint64_t> &queries) {
+  (*layer_queries_callback_)(
+      SecondLayerQeuriesToFirstLayerQueries(queries, fri_step_));
 }
 
 //  ---------------------------------------------------------
@@ -35,24 +37,26 @@ void FriCommittedLayerByCallback::Decommit(const std::vector<uint64_t>& queries)
 
 FriCommittedLayerByTableProver::FriCommittedLayerByTableProver(
     size_t fri_step, MaybeOwnedPtr<const FriLayer> layer,
-    const TableProverFactory& table_prover_factory, const FriParameters& params, size_t layer_num)
-    : FriCommittedLayer(fri_step),
-      fri_layer_(std::move(layer)),
-      params_(params),
-      layer_num_(layer_num) {
+    const TableProverFactory &table_prover_factory, const FriParameters &params,
+    size_t layer_num)
+    : FriCommittedLayer(fri_step), fri_layer_(std::move(layer)),
+      params_(params), layer_num_(layer_num) {
   uint64_t chunk_size = fri_layer_->ChunkSize();
   uint64_t n_chunks = SafeDiv(fri_layer_->LayerSize(), chunk_size);
-  table_prover_ = table_prover_factory(n_chunks, chunk_size / Pow2(fri_step_), Pow2(fri_step_));
-  ASSERT_RELEASE(table_prover_, "No table prover for current layer (probably last layer)");
+  table_prover_ = table_prover_factory(n_chunks, chunk_size / Pow2(fri_step_),
+                                       Pow2(fri_step_));
+  ASSERT_RELEASE(table_prover_,
+                 "No table prover for current layer (probably last layer)");
   Commit();
 }
 
-FriCommittedLayerByTableProver::ElementsData FriCommittedLayerByTableProver::EvalAtPoints(
-    const gsl::span<uint64_t>& required_row_indices) {
+FriCommittedLayerByTableProver::ElementsData
+FriCommittedLayerByTableProver::EvalAtPoints(
+    const gsl::span<uint64_t> &required_row_indices) {
   // Create a vector and a span totally matching. Span cannot be independent.
   // It's needed in order to have the ability of getting subspans.
   ElementsData elements_data;
-  auto& [elements_data_spans, elements_data_vectors] = elements_data;
+  auto &[elements_data_spans, elements_data_vectors] = elements_data;
 
   size_t coset_size = Pow2(params_.fri_step_list[layer_num_]);
   elements_data_vectors.reserve(coset_size);
@@ -76,22 +80,26 @@ void FriCommittedLayerByTableProver::Commit() {
   uint64_t n_chunks = SafeDiv(fri_layer_->LayerSize(), chunk_size);
   auto storage = fri_layer_->MakeStorage();
   for (uint64_t index = 0; index < n_chunks; ++index) {
-    ConstFieldElementSpan chunk = fri_layer_->GetChunk(storage.get(), chunk_size, index);
+    ConstFieldElementSpan chunk =
+        fri_layer_->GetChunk(storage.get(), chunk_size, index);
     table_prover_->AddSegmentForCommitment({chunk}, index, Pow2(fri_step_));
   }
   table_prover_->Commit();
 }
 
-void FriCommittedLayerByTableProver::Decommit(const std::vector<uint64_t>& queries) {
+void FriCommittedLayerByTableProver::Decommit(
+    const std::vector<uint64_t> &queries) {
   std::set<RowCol> layer_data_queries, layer_integrity_queries;
-  NextLayerDataAndIntegrityQueries(
-      queries, params_, layer_num_, &layer_data_queries, &layer_integrity_queries);
+  NextLayerDataAndIntegrityQueries(queries, params_, layer_num_,
+                                   &layer_data_queries,
+                                   &layer_integrity_queries);
   std::vector<uint64_t> required_row_indices =
-      table_prover_->StartDecommitmentPhase(layer_data_queries, layer_integrity_queries);
+      table_prover_->StartDecommitmentPhase(layer_data_queries,
+                                            layer_integrity_queries);
 
   ElementsData elements_data(EvalAtPoints(required_row_indices));
 
   table_prover_->Decommit(elements_data.elements);
 }
 
-}  // namespace starkware
+} // namespace starkware
